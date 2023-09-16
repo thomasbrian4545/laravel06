b8b61223a30c8ae4794f8abc35287cecac090eec
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-16 12:25:02 +0700

    Latihan alter table migration
    Membuat file migration alter_mahasiswas_table

---
+++ database/migrations/2023_09_16_051615_alter_mahasiswas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('mahasiswas', function (Blueprint $table) {
+            $table->renameColumn('nama','nama_lengkap');
+            $table->text('alamat')->after('tanggal_lahir');
+            $table->dropColumn('ipk');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('mahasiswas', function (Blueprint $table) {
+            $table->renameColumn('nama_lengkap', 'nama');
+            $table->dropColumn('alamat');
+            $table->decimal('ipk',3,2)->default(1.00);
+        });
+    }
+};
